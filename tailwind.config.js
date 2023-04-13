792598608cf793abcc14c8f57876f39f91aadaca
Galaczi Arnold <tinkodev@gmail.com>
2023-04-13 10:44:04 +0200

    image path changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'headerImg': "url('/assets/scattered-forcefields.png')",
+        'headerImg': "url('https://firebasestorage.googleapis.com/v0/b/portfolio-8de31.appspot.com/o/images%2Fscattered-forcefields.png?alt=media&token=c9a29445-b9f5-4dba-a73d-186ece7f4f85')",
       }
     },
   },
